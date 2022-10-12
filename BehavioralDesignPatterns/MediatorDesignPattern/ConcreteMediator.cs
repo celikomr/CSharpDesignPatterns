8477880a1c18591d656081269053adadf364f325
Ömer Çelik <oclk@outlook.com.tr>
2022-10-12 22:24:41 +0300

    feat: Added ConcreteMediatorclass to MediatorDesignPattern project

---
+++ BehavioralDesignPatterns/MediatorDesignPattern/ConcreteMediator.cs
@@ -0,0 +1,19 @@
+﻿namespace MediatorDesignPattern;
+
+public class ConcreteMediator : IMediator
+{
+    public ConcreteColleagueA? ConcreteColleagueA { get; set; }
+    public ConcreteColleagueB? ConcreteColleagueB { get; set; }
+
+    public void SendMessage(Colleague colleague, string msg)
+    {
+        if (colleague == ConcreteColleagueA)
+        {
+            ConcreteColleagueB?.Receive(msg);
+        }
+        else
+        {
+            ConcreteColleagueA?.Receive(msg);
+        }
+    }
+}
