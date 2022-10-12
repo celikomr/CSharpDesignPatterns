d4805ebe1e67299f6e5b2a66f7d28e9a0508c7c8
Ömer Çelik <oclk@outlook.com.tr>
2022-10-12 22:34:27 +0300

    feat: Developed sample Program for MediatorDesignPattern project

--- BehavioralDesignPatterns/MediatorDesignPattern/Program.cs
+++ BehavioralDesignPatterns/MediatorDesignPattern/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using MediatorDesignPattern;
+
+ConcreteMediator concreteMediator = new ();
+
+ConcreteColleagueA concreteColleagueA = new (concreteMediator);
+ConcreteColleagueB concreteColleagueB = new (concreteMediator);
+
+concreteMediator.ConcreteColleagueA = concreteColleagueA;
+concreteMediator.ConcreteColleagueB = concreteColleagueB;
+
+concreteColleagueA.Send("Hi, how are you?");
+concreteColleagueB.Send("Fine, thanks and you?");
+concreteColleagueA.Send("I am fine too.");
+
+// Wait for user
+Console.ReadKey();
