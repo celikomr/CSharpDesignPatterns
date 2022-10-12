c964b749635d40e71c2826d686f681c92261b686
Ömer Çelik <oclk@outlook.com.tr>
2022-10-12 22:17:46 +0300

    feat: Added ConcreteColleagueB class to MediatorDesignPattern project

---
+++ BehavioralDesignPatterns/MediatorDesignPattern/ConcreteColleagueB.cs
@@ -0,0 +1,19 @@
+﻿namespace MediatorDesignPattern;
+
+public class ConcreteColleagueB : Colleague
+{
+    public ConcreteColleagueB(IMediator mediator) : base(mediator)
+    {
+    }
+
+    public void Send(string msg)
+    {
+        Console.WriteLine("B send message:" + msg);
+        _mediator.SendMessage(this, msg);
+    }
+
+    public void Receive(string msg)
+    {
+        Console.WriteLine("B receive message:" + msg);
+    }
+}
