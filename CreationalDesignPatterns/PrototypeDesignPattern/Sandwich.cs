b076a29c1c3673504c4a47bd5e4f15703bbbd9a9
Taner Kahraman <taner_kahraman97@hotmail.com>
2022-10-13 16:43:07 +0300

    feat: Added Sandwich class in PrototypeDesignPattern project

---
+++ CreationalDesignPatterns/PrototypeDesignPattern/Sandwich.cs
@@ -0,0 +1,30 @@
+﻿namespace PrototypeDesignPattern;
+
+/// <summary>
+/// The ConcretePrototype class
+/// </summary>
+public class Sandwich : SandwichPrototype
+{
+    private readonly string Bread;
+    private readonly string Meat;
+    private readonly string Cheese;
+    private readonly string Veggies;
+
+    public Sandwich(string bread, string meat, string cheese, string veggies)
+    {
+        Bread = bread;
+        Meat = meat;
+        Cheese = cheese;
+        Veggies = veggies;
+    }
+
+    public override SandwichPrototype? Clone()
+    {
+        return MemberwiseClone() as SandwichPrototype;
+    }
+
+    public override string GetDetails()
+    {
+        return string.Format("(Bread: {0}) - (Meat: {1}) - (Cheese: {2}) - (Veggies: {3})", Bread, Meat, Cheese, Veggies);
+    }
+}
