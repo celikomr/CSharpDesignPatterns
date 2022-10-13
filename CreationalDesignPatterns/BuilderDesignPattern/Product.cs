208e74e6265d5989eca0115b724f609e270dd2d6
Gokhan ALTINKILIT <gokhan.altinkilit@hotmail.com>
2022-10-13 13:59:31 +0300

    feat: Added Product class in BuilderDesignPattern project

---
+++ CreationalDesignPatterns/BuilderDesignPattern/Product.cs
@@ -0,0 +1,28 @@
+﻿namespace BuilderDesignPattern;
+
+/// <summary>
+/// The 'Product' class
+/// </summary>
+public class Product
+{
+    private readonly List<object> _parts = new();
+
+    public void Add(string part)
+    {
+        _parts.Add(part);
+    }
+
+    public string ListParts()
+    {
+        string str = string.Empty;
+
+        for (int i = 0; i < _parts.Count; i++)
+        {
+            str += _parts[i] + ", ";
+        }
+
+        str = str.Remove(str.Length - 2); // removing last ",c"
+
+        return "Product parts: " + str + "\n";
+    }
+}
