e81286d91a31b4209def43c0d099a5df08b1c681
Gokhan ALTINKILIT <gokhan.altinkilit@hotmail.com>
2022-10-13 14:08:40 +0300

    feat: Added ConcreteBuilder class in BuilderDesignPattern project

---
+++ CreationalDesignPatterns/BuilderDesignPattern/ConcreteBuilder.cs
@@ -0,0 +1,45 @@
+﻿namespace BuilderDesignPattern;
+
+/// <summary>
+/// The 'ConcreteBuilder' class
+/// </summary>
+public class ConcreteBuilder : IBuilder
+{
+    private Product _product = new ();
+
+    // A fresh builder instance should contain a blank product object, which
+    // is used in further assembly.
+    public ConcreteBuilder()
+    {
+        Reset();
+    }
+
+    public void BuildPartA()
+    {
+        _product.Add("PartA1");
+    }
+
+    public void BuildPartB()
+    {
+        _product.Add("PartB1");
+    }
+
+    public void BuildPartC()
+    {
+        _product.Add("PartC1");
+    }
+
+    public Product GetProduct()
+    {
+        Product result = _product;
+
+        Reset();
+
+        return result;
+    }
+
+    private void Reset()
+    {
+        _product = new Product();
+    }
+}
